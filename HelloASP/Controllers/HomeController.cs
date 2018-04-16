21b6ebb4bf67d0f37ce606935debb5aa19afc770
Roy Lovitt <lovittl@my.erau.edu>
2018-04-16 13:28:20 -0400

    complete project.

---
+++ HelloASP/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using HelloASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Photos()
+        {
+            var model = new PhotoModel
+            {
+                AltText = "Hello World!",
+                URL = "http://www.example.com/itDoesntexist"
+            };
+            return View(model);
+        }
+    }
+}
---
+++ HelloASP/Models/PhotoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloASP.Models
+{
+    public class PhotoModel
+    {
+        public string AltText { get; set; }
+        public string URL { get; set; }
+    }
+}
